e8e33d739867d7747bf39328e06550b7baf322ca
CaePolit <caepolit@gmail.com>
2024-08-02 15:43:18 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplicationIdentity/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationIdentity.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+
+        public IEnumerable<string> Get()
+
+        {
+
+            return new string[] { "Le One Piece est..", "... Au milieu de l'ocean Pacific" };
+
+        }
+    }
+}
